76bf64a181901011972f707ce0202c12b698f3cb
小川貴大 <mistolteen0127@gmail.com>
2019-08-30 00:05:20 +0900

    呼び出した結果を表示するサンプル

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import axios from "axios"
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    key1: "",
+    key2: ""
+  }
+
+  handleClick = async () => {
+    try {
+      const response = await axios.get("http://localhost:4567/")
+      const data = response.data
+      this.setState({ key1: data.key1, key2: data.key2 })
+    } catch (e) {
+      alert(e)
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button style={{ width: 200, height: 35 }} onClick={this.handleClick}>
+          Sinatra呼び出し
+        </button>
+        <p>{this.state.key1}</p>
+        <p>{this.state.key2}</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
